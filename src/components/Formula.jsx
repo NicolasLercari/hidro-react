533cfd6c6fe1d22205abbe9ec248d670dee84d77
NicolasLercari <nicolaslercari@gmail.com>
2019-06-27 00:54:02 -0300

    balance total funcionando [debugear]

--- src/components/Formula.jsx
+++ src/components/Formula.jsx
@@ -9,6 +9,8 @@ import InputCantidad from './InputCantidad.jsx';
 import Button from '@material-ui/core/Button';
 import FormulaTable from './FormulaTable.jsx';
 
+import { getIngredienteObjectByName } from './ingredientesHelper.js';
+
 const buttonStyles = {margin: '2px', backgroundColor: '#3f51b5', borderColor: '#3f51b5'};
 
 
@@ -21,13 +23,12 @@ class Formula extends Component {
     }
   }
 
-  actualizarBalanceTotal = () => {
-    const { ingredientesAgregados } = this.state;
+  actualizarBalanceTotal = ingredientesAgregados => {
     let total = 0;
     if (ingredientesAgregados.length){
       total =  ingredientesAgregados.map(e => Number(e.cantidad)).reduce((x,y) => x+y);
     }
-    const ingredientesConTotales = this.obtenerPropiedadesConTotales(total);
+    const ingredientesConTotales = this.obtenerPropiedadesConTotales(ingredientesAgregados, total);
     //const ingredientesProporcion = ingredientesAgregados.map(e => Number(e.cantidad)/total);
     //const ingredientesConTotales = this.obtener(ingredientesProporcion, total);
 
@@ -76,12 +77,14 @@ class Formula extends Component {
     const index = ingredientesAgregados.indexOf(oldIngrediente);
     ingredientesAgregados[index] = newIngrediente;
     this.setState({ ingredientesAgregados }, callback);
+    this.actualizarBalanceTotal(ingredientesAgregados);
   }
 
   onAdd = (newIngrediente, callback) => {
     const { ingredientesAgregados } = this.state;
     ingredientesAgregados.push(newIngrediente);
     this.setState({ ingredientesAgregados }, callback);
+    this.actualizarBalanceTotal(ingredientesAgregados);
   }
 
   onDelete = (oldIngrediente, callback) => {
@@ -89,6 +92,7 @@ class Formula extends Component {
     const index = ingredientesAgregados.indexOf(oldIngrediente);
     ingredientesAgregados.splice(index, 1);
     this.setState({ ingredientesAgregados }, callback);
+    this.actualizarBalanceTotal(ingredientesAgregados);
   }
 
   obtenerTotalParaPropiedad = (ingredientes, propiedad) => {
@@ -104,17 +108,16 @@ class Formula extends Component {
     this.setState({ ingrediente })
   }
 
-  obtenerPropiedadesConTotales = (total) => {
-    const { ingredientesAgregados } = this.state;
+  obtenerPropiedadesConTotales = (ingredientes, total) => {
     let ingredientesConTotales = [];
-    for(let i=0; i < ingredientesAgregados.length; i++){
-      const ingrediente = ingredientesAgregados[i];
+    for(let i=0; i < ingredientes.length; i++){
+      const ingrediente = getIngredienteObjectByName(ingredientes[i].INGREDIENTES);
       const propiedades = Object.keys(ingrediente)
                         .filter(n => n !== 'INGREDIENTES' &&  n !== 'key' && n !== 'cantidad');
       const ingredienteConTotal = Object.assign({}, propiedades);
       for(let j=0; j < propiedades.length; j++){
         // divido por el total para obtener el porcentaje que muestra el excel
-        ingredienteConTotal[propiedades[j]] = (Number(ingrediente.cantidad)/total) * (ingredientesAgregados[i][propiedades[j]]);
+        ingredienteConTotal[propiedades[j]] = (Number(ingredientes[i].cantidad)/total) * (ingrediente[propiedades[j]]);
       };
       ingredientesConTotales.push(ingredienteConTotal);
     }
